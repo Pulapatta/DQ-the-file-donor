5161c5e545fd07787e7e4c51e2d6955f54ec6395
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-06-10 15:45:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1609,55 +1609,55 @@ async def auto_filter(client, msg, spoll=False):
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
             try:
                 if settings['auto_delete']:
-                    await asyncio.sleep(600)
+                    await asyncio.sleep(DTIME)
                     await hehe.delete()
-                    await message.delete()
+                   # await message.delete()
             except KeyError:
                 await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(600)
+                await asyncio.sleep(DTIME)
                 await hehe.delete()
-                await message.delete()
+               # await message.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
             hmm = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
             try:
                 if settings['auto_delete']:
-                    await asyncio.sleep(600)
+                    await asyncio.sleep(DTIME)
                     await hmm.delete()
-                    await message.delete()
+                   # await message.delete()
             except KeyError:
                 await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(600)
+                await asyncio.sleep(DTIME)
                 await hmm.delete()
-                await message.delete()
+                #await message.delete()
         except Exception as e:
             logger.exception(e)
             fek = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             try:
                 if settings['auto_delete']:
-                    await asyncio.sleep(600)
+                    await asyncio.sleep(DTIME)
                     await fek.delete()
-                    await message.delete()
+                   # await message.delete()
             except KeyError:
                 await save_group_settings(message.chat.id, 'auto_delete', True)
-                await asyncio.sleep(600)
+                await asyncio.sleep(DTIME)
                 await fek.delete()
-                await message.delete()
+            #    await message.delete()
     else:
         fuk = await message.reply_photo(photo=NOR_IMG, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         try:
             if settings['auto_delete']:
-                await asyncio.sleep(600)
+                await asyncio.sleep(DTIME)
                 await fuk.delete()
-                await message.delete()
+             #   await message.delete()
         except KeyError:
             await save_group_settings(message.chat.id, 'auto_delete', True)
-            await asyncio.sleep(600)
+            await asyncio.sleep(DTIME)
             await fuk.delete()
-            await message.delete()
-    if spoll:
-        await msg.message.delete()
+          #  await message.delete()
+   # if spoll:
+    #    await msg.message.delete()
 
 
 async def advantage_spell_chok(client, msg):
